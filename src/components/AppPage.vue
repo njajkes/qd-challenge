569a24c19cb8e15c30d9b2f8b95265ec6e3c3434
njajkes <naikes.nikolait@yandex.ru>
2023-06-22 16:26:36 +0300

    feat: added task form

--- src/components/AppPage.vue
+++ src/components/AppPage.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import { useStore } from 'vuex';
+import { Save } from '../store';
+import TaskForm from './TaskForm.vue'
+
+const store = useStore()
+window.addEventListener('beforeunload', () => {
+  Save(store)
+})
+
 </script>
 
 <template>
 <h1>App</h1>
+<TaskForm />
+<div>list</div>
 </template>
 
 <style scoped>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useStore } from 'vuex';
+import { commitAddTask } from '../store';
+
+const title = ref<string>('')
+const store = useStore()
+
+const onSubmit = (e: Event) => {
+  e.preventDefault()
+  if (title.value) {
+    commitAddTask(store, title.value)
+    title.value = ''
+  }
+}
+
+</script>
+
+<template>
+<div class="form-wrapper">
+  <h3>Enter new task</h3>
+  <form @submit.prevent="onSubmit">
+    <input v-model.trim="title" />
+    <button>Submit</button> 
+  </form>
+</div>
+</template>
+
+<style scoped>
+.form-wrapper {
+  margin-bottom: 15px;
+}
+</style>
